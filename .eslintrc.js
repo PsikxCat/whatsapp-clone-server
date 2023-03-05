eabd38cb9143f785cadbf7b8a0a262070f3b4827
PsikxCat <arevalogrichard@gmail.com>
2023-03-05 10:58:09 -0500

    :heavy_plus_sign: package.json

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true
+  },
+  extends: [
+    'standard'
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  rules: {
+    semi: ['error', 'always'],
+    'comma-dangle': ['error', 'only-multiline'],
+    'space-before-function-paren': ['error', {
+      anonymous: 'always',
+      named: 'never',
+      asyncArrow: 'always'
+    }]
+  }
+};
